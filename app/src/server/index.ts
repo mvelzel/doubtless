e774304302b0d598adbe54b27289253f3e3a1721
Meine Velzel <mvelzel@DESKTOP-1GHRAP8>
2024-11-07 15:10:54 +0100

    Sets up server code

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -1,2 +1,34 @@
-import * as trpc from '@trpc/server';
+import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { db } from "./db";
+import { publicProcedure, router } from './trpc';
+import { z } from 'zod';
 
+const appRouter = router({
+  userList: publicProcedure
+    .query(async () => {
+      const users = await db.user.findMany();
+      return users;
+    }),
+  userById: publicProcedure
+    .input(z.string())
+    .query(async (opts) => {
+      const { input } = opts;
+      const user = await db.user.findById(input);
+      return user;
+    }),
+  userCreate: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async (opts) => {
+      const { input } = opts;
+      const user = await db.user.create(input);
+      return user;
+    }),
+});
+
+export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
