9da8a118405d174bc7bd52391e9b2be6e445fdbf
Meine Velzel <mvelzel@DESKTOP-1GHRAP8>
2024-11-09 00:05:53 +0100

    Makes TRPC POC and add Tailwind JIT

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { trpc } from './utils/trpc'
+import { httpBatchLink } from '@trpc/react-query'
+import './main.css'
+
+const queryClient = new QueryClient();
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/'
+    })
+  ]
+});
 
 // Set up a Router instance
 const router = createRouter({
@@ -20,5 +34,14 @@ const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+
+  root.render(
+    <StrictMode>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </trpc.Provider>
+    </StrictMode>
+  )
 }
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,14 +1,29 @@
-import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { trpc } from '../utils/trpc'
 
 export const Route = createFileRoute('/')({
   component: HomeComponent,
 })
 
 function HomeComponent() {
+  const utils = trpc.useUtils();
+
+  const usersQuery = trpc.userList.useQuery();
+  const userCreator = trpc.userCreate.useMutation({
+    onSuccess(_) {
+      utils.userList.invalidate();
+    }
+  });
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+
+      <button onClick={() => userCreator.mutate({ name: 'Frodo' })}>
+        Create User!
+      </button>
+
+      {usersQuery.data?.map(user => (<p key={user.id}>{user.id}: {user.name}</p>))}
     </div>
   )
 }
