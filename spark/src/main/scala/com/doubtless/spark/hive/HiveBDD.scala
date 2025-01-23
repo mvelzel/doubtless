76a4f20cf816e45c594701c68860b97e17791864
Meine Velzel <mvelzel@Meines-MacBook-Pro.local>
2025-01-23 22:04:08 +0100

    Implement more Hive BDD UDFs

--- spark/src/main/scala/com/doubtless/spark/hive/HiveBDD.scala
+++ spark/src/main/scala/com/doubtless/spark/hive/HiveBDD.scala
@@ -14,7 +14,38 @@ class HiveBDD extends UDF {
 
 class HiveBDDToString extends UDF {
   def evaluate(input: BytesWritable): String = {
+    if (input == null) return null
+
     val bdd = new BDD(input.getBytes())
     bdd.toString()
+  }
+}
+
+class HiveBDDAnd extends UDF {
+  def evaluate(left: BytesWritable, right: BytesWritable): Array[Byte] = {
+    if (left == null || right == null) return null
+
+    val leftBdd = new BDD(left.getBytes())
+    val rightBdd = new BDD(right.getBytes())
+    (leftBdd & rightBdd).buffer
+  }
+}
+
+class HiveBDDOr extends UDF {
+  def evaluate(left: BytesWritable, right: BytesWritable): Array[Byte] = {
+    if (left == null || right == null) return null
+
+    val leftBdd = new BDD(left.getBytes())
+    val rightBdd = new BDD(right.getBytes())
+    (leftBdd | rightBdd).buffer
+  }
+}
+
+class HiveBDDNot extends UDF {
+  def evaluate(input: BytesWritable): Array[Byte] = {
+    if (input == null) return null
+
+    val bdd = new BDD(input.getBytes())
+    (~bdd).buffer
   }
 }
