1ba6697b6494229fddc86893fc2bead7f62a2708
Meine Velzel <m.velzel@hotmail.com>
2025-02-10 17:06:17 +0100

    Use existing ProbCountUDAF implementation

--- spark/src/main/scala/com/doubtless/spark/hive/HiveProbCountUDAF.scala
+++ spark/src/main/scala/com/doubtless/spark/hive/HiveProbCountUDAF.scala
@@ -4,6 +4,7 @@ import com.doubtless.bdd.BDD
 import org.apache.hadoop.hive.ql.exec.{UDAF, UDAFEvaluator}
 import org.apache.hadoop.io.{BytesWritable, IntWritable}
 import scala.jdk.CollectionConverters._
+import com.doubtless.spark.ProbCountUDAF
 
 /*
 Example SQL:
@@ -18,50 +19,31 @@ select key, bdd_to_string(value) from grouped lateral view explode(map) explodeV
 class HiveProbCountUDAF extends UDAF {}
 object HiveProbCountUDAF {
   class HiveProbCountEvaluator extends UDAFEvaluator {
-    var agg: Map[Int, BDD] = Map[Int, BDD]((0 -> BDD.True))
+    var agg: Map[Int, BDD] = ProbCountUDAF.zero
 
     override def init() = {
-      this.agg = Map[Int, BDD]((0 -> BDD.True))
+      this.agg = ProbCountUDAF.zero
     }
 
     def iterate(bytes: BytesWritable) = {
       val inputBdd = new BDD(bytes.getBytes())
-      this.agg = agg.map({ case (count, bdd) =>
-        agg get (count + 1) match {
-          case Some(accBdd) =>
-            (count + 1) -> ((bdd & inputBdd) | (accBdd & ~inputBdd))
-          case None => (count + 1) -> (bdd & inputBdd)
-        }
-      }) + (0 -> (agg(0) & ~inputBdd))
+      this.agg = ProbCountUDAF.reduce(this.agg, inputBdd)
+      true
+    }
+
+    def merge(inter: java.util.Map[IntWritable, BytesWritable]) = {
+      val otherAgg = inter.asScala
+        .map({ case (count, bytes) =>
+          (count.get()) -> new BDD(bytes.getBytes())
+        })
+        .toMap
+
+      this.agg = ProbCountUDAF.merge(this.agg, otherAgg)
       true
     }
 
     def terminatePartial(): java.util.Map[IntWritable, BytesWritable] =
       terminate()
-
-    def merge(inter: java.util.Map[IntWritable, BytesWritable]) = {
-      val scalaInter = inter.asScala
-      val totalMax = this.agg.keys.max + scalaInter.keys.max.get()
-      val otherAgg = scalaInter.map({ case (count, bytes) =>
-        (count) -> new BDD(bytes.getBytes())
-      })
-
-      this.agg = Map(0 to totalMax map { count =>
-        (count ->
-          (0 to count)
-            .map(i =>
-              scalaInter get (new IntWritable(i)) match {
-                case Some(bytes) =>
-                  (this.agg getOrElse (count - i, BDD.False)) & (new BDD(
-                    bytes.getBytes()
-                  ))
-                case None => BDD.False
-              }
-            )
-            .reduce((bdd1: BDD, bdd2: BDD) => bdd1 | bdd2))
-      }: _*)
-      true
-    }
 
     def terminate(): java.util.Map[IntWritable, BytesWritable] =
       this.agg
