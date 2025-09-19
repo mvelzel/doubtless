dec3f8f5ac9f0690fce4be45f0f1517b123d7416
Meine Velzel <m.velzel@hotmail.com>
2025-09-19 15:12:16 +0200

    Drop extension first

--- dbt/macros/udfs/create_pgbdd_extension.sql
+++ dbt/macros/udfs/create_pgbdd_extension.sql
@@ -1,6 +1,7 @@
 {% macro create_pgbdd_extension() %}
 
     {% if target.name == 'postgres' %}
+        drop extension if exists pgbdd cascade;
         create extension if not exists pgbdd;
     {% else %}
         select 1;
